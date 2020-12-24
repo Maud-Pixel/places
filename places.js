a8065be5a50c369109d1cd383085dd49544a8888
Maud-Pixel <leleuxmaud@gmail.com>
2020-12-24 18:22:17 +0100

    first commit really

---
+++ places.js
@@ -0,0 +1,34 @@
+
+//______________clock ________________________//
+
+var hour = document.querySelectorAll(".hours");
+var minute = document.querySelectorAll(".minutes");
+
+function get_clock()
+{
+    var time = new Date();
+    var time_hour = time.getHours();
+    var time_minute = time.getMinutes();
+    hour.forEach(item => 
+    {
+        if (time_hour < 10)
+        {item.innerText = "0"+time_hour+":";}
+        else 
+        { item.innerText = time_hour +":";}
+    })
+    minute.forEach(item=>
+        {
+            if (time_minute < 10)
+            {
+            item.innerText = "0"+time_minute;
+            }
+            else 
+            {item.innerText = time_minute;}
+        })
+        
+}
+
+
+setInterval(get_clock(), 1000);
+
+//_____________carrousel________________
